8c90504399b6148a447d828bb277592c1a963d94
Sean Loyless <sean.casey.loyless@ibm.com>
2019-06-14 13:39:34 -0500

    feat(structure): update structure to Carbon

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './styles/styles.scss';
+import { Content } from 'carbon-components-react/lib/components/UIShell';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Content>
+          <div>Hello Bitch!</div>
+        </Content>
+      </>
+    );
+  }
+}
+
+export default App;
